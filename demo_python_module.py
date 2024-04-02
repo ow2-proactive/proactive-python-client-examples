e861724a1d987affa2fc41d15bd8f4860ce47a49
Andrews Cordolino Sobral <andrewssobral@gmail.com>
2024-04-02 14:57:58 +0200

    Updated demo_python_module, no more need "sys.path.append(os.getcwd())" , uses the new version of python engine

--- demo_python_module.py
+++ demo_python_module.py
@@ -22,9 +22,6 @@ task = gateway.createPythonTask("demo_python_module_task")
 task.addGenericInformation("PYTHON_COMMAND", "python3")
 task.addInputFile('demo_python_module/**')
 task.setTaskImplementation("""
-import os, sys
-sys.path.append(os.getcwd())  # Required to avoid ModuleNotFoundError
-
 from demo_python_module.mymodule import add_numbers
 x=1
 y=2
