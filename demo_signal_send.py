4496e9d037004893bfa6371b8b9d342e89d19e97
Andrews Cordolino Sobral <andrewssobral@users.noreply.github.com>
2024-07-03 17:21:00 +0200

    Updated demo_signal_wait.py , now it uses the new setSignals method (#2)
    * Updated demo_signal_wait.py , now it uses the new setSignals method
    
    * Added demo_signal_wait2.py showing how to use signals on postscript
    
    * Renaming demo_signal_send.py to demo_signal_send_job.py
    
    * Adding a demo for sendSignals function
    
    ---------
    
    Co-authored-by: dianajlailaty <diana.jlailaty@activeeon.com>

--- demo_signal_send.py
+++ demo_signal_send.py
@@ -1,90 +1,42 @@
 """
 ProActive Signal API Demonstration
 
-This script demonstrates how to submit a Python job to the ProActive Scheduler and send a 'Stop' signal to a specified job.
+This script demonstrates how to send a signal to a specified job with or without variables.
 
 Overview:
-1. Handle command-line arguments to retrieve the target job ID.
+1. Handle command-line arguments to retrieve the target job ID, signal name, and variables dictionary.
 2. Initialize the ProActive Scheduler gateway.
-3. Create a ProActive job for sending signals.
-4. Create a task to send the 'Stop' and 'Continue' signals using a bash script.
-5. Add a pre-script to the task to set session ID and ProActive URL.
-6. Submit the job to the ProActive Scheduler.
-7. Retrieve and display the job output.
-8. Ensure proper cleanup by disconnecting from the ProActive Scheduler gateway post-execution.
+3. Send the signals using the gateway's sendSignal function.
+4. Ensure proper cleanup by disconnecting from the ProActive Scheduler gateway post-execution.
 
 Documentation:
 - https://doc.activeeon.com/latest/javadoc/org/ow2/proactive/scheduler/signal/Signal.html
 """
+
 import argparse
+import json
 from proactive import getProActiveGateway, ProactiveScriptLanguage
 
-# Step 1: Set up argparse to handle command-line arguments
+# Set up argparse to handle command-line arguments
 parser = argparse.ArgumentParser(description='Submit a Python task to the ProActive Scheduler.')
-parser.add_argument('--target_job_id', required=True, type=str, help='The target job ID to send the stop signal to')
+parser.add_argument('--target_job_id', required=True, type=str, help='The target job ID to send the signal to')
+parser.add_argument('--signal_name', required=True, type=str, help='The name of the signal to be sent')
+parser.add_argument('--variables', required=False, type=str, help='The dictionary of variables to be sent along with the signal.')
 args = parser.parse_args()
 
-# Retrieve the target_job_id from command-line arguments
-target_job_id = args.target_job_id
+# Retrieve the target_job_id, signal_name, and variables from command-line arguments
+target_job_id = int(args.target_job_id)
+signal_name = args.signal_name
+if(args.variables):
+    variables = json.loads(args.variables)
+else:
+    variables = {}
 
 # Initialize the ProActive gateway
 gateway = getProActiveGateway()
 
-# Create a job to send signal and variables to the previous job
-print("Creating a proactive job to send stop signal to the previous job...")
-
-# Create a new job
-job = gateway.createJob("demo_signal_send_job")
-
-# Create a bash task to send the stop signal
-send_signal_task = gateway.createTask(ProactiveScriptLanguage().linux_bash(), "send_signal_task")
-
-# Set the task implementation to send the stop signal using curl
-send_signal_task.setTaskImplementation(f""" 
-sessionid=$variables_SESSION_ID
-proactiveurl=$variables_PROACTIVE_URL
-target_job_id={target_job_id}
-
-# Send the Continue signal with variables
-curl -X POST \\
--H "sessionid:$sessionid" \\
--H "Content-Type: application/json" \\
--d '{{"BOOLEAN_VARIABLE": "false", "INTEGER_VARIABLE": 24}}' \\
-"$proactiveurl/rest/scheduler/job/$target_job_id/signals?signal=Continue"
-
-# Send the Stop signal
-# curl -X POST -H "sessionid:$sessionid" $proactiveurl/rest/scheduler/job/$target_job_id/signals?signal="Stop"
-""")
-
-# Add a pre-script to the task to set session ID and the ProActive URL
-print("Adding a pre-script to task...")
-pre_script = gateway.createPreScript(ProactiveScriptLanguage().groovy())
-pre_script.setImplementation("""
-schedulerapi.connect()
-def sessionId = schedulerapi.getSession()
-
-def connectionInfo = schedulerapi.getConnectionInfo()
-String ciUrl = connectionInfo.getUrl()
-def url = new URL(ciUrl)
-def proactiveUrl = url.getProtocol() + "://" + url.getHost() + ":" + url.getPort()
-
-variables.put("SESSION_ID", sessionId) 
-variables.put("PROACTIVE_URL", proactiveUrl)
-""")
-send_signal_task.setPreScript(pre_script)
-
-# Add the task to the job
-job.addTask(send_signal_task)
-
-# Submit the job to the proactive scheduler
-print("Submitting the send signal job to the proactive scheduler...")
-job_id = gateway.submitJob(job)
-print("job_id: " + str(job_id))
-
-# Retrieve job output
-print("Getting job output...")
-job_output = gateway.getJobOutput(job_id)
-print(job_output)
+# Send signal to the job with target_job_id
+gateway.sendSignal(target_job_id,signal_name,variables)
 
 # Cleanup
 gateway.close()
